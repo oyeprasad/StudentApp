800e8c7fd0ef057e38949c5f340dab58893e2e49
oyeprasad <amitprasad33@gmail.com>
2020-01-23 00:07:43 +0530

    Questions Api implemented

--- Assets/Scripts/Home/Quiz/QuizController.cs
+++ Assets/Scripts/Home/Quiz/QuizController.cs
@@ -12,6 +12,10 @@ using UnityEngine.Networking;
     public string question = "";
     public List<Option> options = new List<Option>();
 }
+[Serializable] public class QuestionsRoot
+{
+    public List<Question> questions = new List<Question>(); 
+}
 [Serializable] public class Option
 {
     public int option_id = 0;
@@ -19,10 +23,14 @@ using UnityEngine.Networking;
     public string correct_incorrect = "";
 }
 
+[Serializable] public class GetQuetionResponse : ResponseBase
+{
+    public QuestionsRoot data = new QuestionsRoot();
+}
+
 public class QuizController : MonoBehaviour
 {
-    [SerializeField] private Text queNumberText;
-    [SerializeField] private Text queHeaderText;
+    [SerializeField] private Text queNumberText; 
     [SerializeField] private Text queBodyText;
     [SerializeField] private Text optionA_Text;
     [SerializeField] private Text optionB_Text;
@@ -31,21 +39,31 @@ public class QuizController : MonoBehaviour
 
     [SerializeField] private GameObject greateJobPanel;
     [SerializeField] private GameObject tryAgainJobPanel;
+     
 
-    private List<Question> question = new List<Question>();
+    private List<Question> AllAvailableQuestions = new List<Question>();
     public void OnOptionClick(int optionId)
     {
-
+        if(optionId == currectOptionIndex)
+        {
+            greateJobPanel.SetActive(true);
+        }
+         else
+        {
+           tryAgainJobPanel.SetActive(true); 
+        }
     }
 
 
     //-----------------------
     public void PopulateQuizzesOnVideo(int videoId)
     {
+        ClearAllText();
         StartCoroutine(LoadQuestions("video", videoId)); 
     }
     public void PopulateQuizzesOnSubCat(int subcatId)
     {
+        ClearAllText();
         StartCoroutine(LoadQuestions("subcat", subcatId));
     }
 
@@ -74,19 +92,87 @@ public class QuizController : MonoBehaviour
                 yield return webRequest;
             if (webRequest.isNetworkError)
             { 
-                
+                HomeMainUIController.ShowPopup.Invoke("Oops! Some error in loading questions. Please try again later", () => print("Some error"));
             }
             else
             {
-                print(webRequest.downloadHandler.text);       
+                print(webRequest.downloadHandler.text);     
+                GetQuetionResponse response = JsonUtility.FromJson<GetQuetionResponse>(webRequest.downloadHandler.text);
+                if(response.status)
+                {
+                    if(response.data.questions.Count <= 0)
+                    {
+                        HomeMainUIController.ShowPopup.Invoke("No Questions available on this topic.", () => HomeMainUIController.EventBackClicked.Invoke());    
+                    } else
+                    {
+                            AllAvailableQuestions = response.data.questions;
+                            print(AllAvailableQuestions.Count);
+                            print(AllAvailableQuestions[0].options.Count);
+                            PopulateNextQuestion();
+                    }
+                }
+                else
+                {
+                    HomeMainUIController.ShowPopup.Invoke(response.message, () => HomeMainUIController.EventBackClicked.Invoke());
+                }
+
             } 
             
-        }
-
-
+        } 
     }
 
+    int queIndex = 0;
+    int currectOptionIndex = 0;
+    private void PopulateNextQuestion()
+    {
+        Question nextQuestion = AllAvailableQuestions[queIndex];
+        queNumberText.text = nextQuestion.question_id.ToString();
+        queBodyText.text = nextQuestion.question;
+        optionA_Text.text = nextQuestion.options[0].option.ToString();
+        optionB_Text.text = nextQuestion.options[1].option.ToString();
+        optionC_Text.text = nextQuestion.options[2].option.ToString();
+        optionD_Text.text = nextQuestion.options[3].option.ToString();
 
+        for(int i = 0; i < nextQuestion.options.Count; i++){
+            if(nextQuestion.options[i].correct_incorrect == "C")
+            {
+               currectOptionIndex = i+1; 
+            }
+        }
+        print("Current option is "+currectOptionIndex);
+        queIndex += 1;
+    }
+    public void ButtonGreatJob()
+    {
+        greateJobPanel.SetActive(false);
+        if(AllAvailableQuestions.Count > queIndex)
+        {    
+            PopulateNextQuestion();
+        } 
+        else
+        {
+            HomeMainUIController.ShowPopup.Invoke("Hurray! No more questions.", () =>
+                                            { 
+                                                queIndex = 0;
+                                                ClearAllText();
+                                                HomeMainUIController.EventBackClicked.Invoke();
+                                            });
+        }
+    }
+    public void ButtonTryAgain()
+    {
+        tryAgainJobPanel.SetActive(false);
+    }
+
+    void ClearAllText()
+    {
+        queNumberText.text = "";
+        queBodyText.text = "";
+        optionA_Text.text = "";
+        optionB_Text.text = "";
+        optionC_Text.text = "";
+        optionD_Text.text = "";
+    }
     
 
 }
